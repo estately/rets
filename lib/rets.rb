25f75998ea36e17c4b2e4c7413e2869f6710f107
Andrew Smith <andy@tinnedfruit.org>
2011-03-24 13:24:08 -0700

    Adding constructor.

--- lib/rets.rb
+++ lib/rets.rb
@@ -1,3 +1,28 @@
-class Rets
+require 'net/http'
+require 'uri'
+require 'cgi'
+require 'digest/md5'
+
+require 'rubygems'
+require 'net/http/persistent'
+
+module Rets
   VERSION = '0.0.1'
+
+  class Client
+
+    DEFAULT_OPTIONS = { :persistent => true }
+
+    attr_accessor :uri, :options
+
+    def initialize(options)
+      uri          = URI.parse(options[:login_url])
+
+      uri.user     = options.key?(:username) ? CGI.escape(options[:username]) : nil
+      uri.password = options.key?(:password) ? CGI.escape(options[:password]) : nil
+
+      self.options = DEFAULT_OPTIONS.merge(options)
+      self.uri     = uri
+    end
+  end
 end
--- test/test_rets.rb
+++ test/test_rets.rb
@@ -2,7 +2,36 @@ require "test/unit"
 require "rets"
 
 class TestRets < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+
+  def test_initialize_adds_escaped_username_to_uri
+    client = Rets::Client.new(
+      :login_url => "http://example.com",
+      :username => "bob@example.com")
+
+    assert_equal CGI.escape("bob@example.com"), client.uri.user
+    assert_nil client.uri.password
+  end
+
+  def test_initialize_adds_escaped_password_to_uri
+    client = Rets::Client.new(
+      :login_url => "http://example.com",
+      :username => "bob",
+      :password => "secret@2!")
+
+    assert_equal CGI.escape("secret@2!"), client.uri.password
+  end
+
+  def test_initialize_merges_default_options
+    client = Rets::Client.new(:login_url => "http://example.com", :foo => true)
+
+    assert client.options.include?(:foo)
+  end
+
+  def test_initialize_allows_options_to_be_overridden
+    client = Rets::Client.new(:login_url => "http://example.com")
+    assert_equal true, client.options[:persistent]
+
+    client = Rets::Client.new(:login_url => "http://example.com", :persistent => false)
+    assert_equal false, client.options[:persistent]
   end
 end
