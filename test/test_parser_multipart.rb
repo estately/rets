a37f1b9f4f0a7bdadbb9c2563959c1d70a74f034
Doug Cole <doug@estately.com>
2012-03-16 15:21:26 -0700

    move error_checking to helper use for multipart parsing

--- test/test_parser_multipart.rb
+++ test/test_parser_multipart.rb
@@ -28,4 +28,12 @@ class TestParserMultipart < Test::Unit::TestCase
     parts = Rets::Parser::Multipart.parse(MULTIPART_RESPONSE_URLS, "rets.object.content.boundary.1330546052739")
     assert_equal 'http://foobarmls.com/RETS//MediaDisplay/98/hr2890998-1.jpg', parts[0].headers['location']
   end
+
+  def test_parse_with_error
+    raw = "\r\n--89467f8e0c6b48158c8f1883910212ec\r\nContent-Type: text/xml\r\nContent-ID: foo\r\nObject-ID: *\r\n\r\n<RETS ReplyCode=\"20403\" ReplyText=\"No Object Found\" />\r\n\r\n--89467f8e0c6b48158c8f1883910212ec--\r\n"
+    boundary = "89467f8e0c6b48158c8f1883910212ec"
+    assert_raise Rets::InvalidRequest do
+      Rets::Parser::Multipart.parse(raw, boundary)
+    end
+  end
 end
