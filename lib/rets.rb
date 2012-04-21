6b33886ca8c0d4382e026aaee7430e5a85cca78b
Doug Cole <doug@estately.com>
2012-04-20 23:17:38 -0700

    bump version and update changelog

--- lib/rets.rb
+++ lib/rets.rb
@@ -8,7 +8,7 @@ require 'net/http/persistent'
 require 'nokogiri'
 
 module Rets
-  VERSION = '0.1.7'
+  VERSION = '0.2.0'
 
   AuthorizationFailure = Class.new(ArgumentError)
   InvalidRequest       = Class.new(ArgumentError)
