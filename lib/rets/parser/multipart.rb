11b738041ec60f6b924963e963f380943889d86d
Trung Duc Tran <trung@tdtran.org>
2012-11-09 16:06:17 +0100

    Reformatting

--- lib/rets/parser/multipart.rb
+++ lib/rets/parser/multipart.rb
@@ -5,29 +5,24 @@ module Rets
     class Multipart
       CRLF = "\r\n"
       WSP  = "\s"
-
       HEADER_LINE = /^([!-9;-~]+:\s*.+)$/
 
       Part = Struct.new(:headers, :body)
 
       def self.parse(raw, boundary)
         parts = []
-
         boundary_regexp = /--#{Regexp.quote(boundary)}(--)?#{CRLF}/
+        raw.split(boundary_regexp).each do |chunk|
+          header_part, body_part = chunk.split(/#{CRLF}#{WSP}*#{CRLF}/m, 2)
 
-          raw.split(boundary_regexp).each do |chunk|
-
-            header_part, body_part = chunk.split(/#{CRLF}#{WSP}*#{CRLF}/m, 2)
-
-            if header_part =~ HEADER_LINE
-              headers = header_part.split(/\r\n/).map { |kv| p = kv.split(/:\s?/); [p[0].downcase, p[1..-1].join(':')] }
-              headers = Hash[*headers.flatten]
-
-              parts << Part.new(headers, body_part)
-            else
-              next # not a valid chunk.
-            end
+          if header_part =~ HEADER_LINE
+            headers = header_part.split(/\r\n/).map { |kv| p = kv.split(/:\s?/); [p[0].downcase, p[1..-1].join(':')] }
+            headers = Hash[*headers.flatten]
+            parts << Part.new(headers, body_part)
+          else
+            next # not a valid chunk.
           end
+        end
         check_for_invalids_parts!(parts)
         parts
       end
