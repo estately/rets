3bc15a7e305ae2d7e61886d35e3457f34db174c6
Tom Crayford <tcrayford@googlemail.com>
2013-06-07 12:16:54 +0100

    pass options through to the locker

--- lib/rets/locking_client.rb
+++ lib/rets/locking_client.rb
@@ -1,14 +1,15 @@
 module Rets
   class LockingClient
-    def initialize(client, locker, lock_name)
+    def initialize(client, locker, lock_name, options={})
       @client = client
       @locker = locker
       @lock_name = lock_name
+      @options = {}
     end
 
     def method_missing(method_name, *args, &block)
       result = nil
-      @locker.lock(@lock_name) do
+      @locker.lock(@lock_name, @options) do
         result = @client.send(method_name, *args, &block)
       end
       result
