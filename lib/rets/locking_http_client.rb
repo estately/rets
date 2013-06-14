bcb1d0674e02ce875a1e22223ee61b5063b3510e
Tom Crayford <tcrayford@googlemail.com>
2013-06-14 13:48:23 +0100

    add locking http client

---
+++ lib/rets/locking_http_client.rb
@@ -0,0 +1,34 @@
+module Rets
+  class LockingHttpClient
+    def initialize(http_client, locker, lock_name, options={})
+      @http_client = http_client
+      @locker = locker
+      @lock_name = lock_name
+      @options = {}
+    end
+
+    def http_get(url, params=nil, extra_headers={})
+      lock_around do
+        @http_client.http_get(url, params, extra_headers)
+      end
+    end
+
+    def http_post(url, params, extra_headers = {})
+      lock_around do
+        @http_client.http_post(url, params, extra_headers)
+      end
+    end
+
+    def save_cookie_store(force=nil)
+      @http_client.save_cookie_store(force)
+    end
+
+    def lock_around(&block)
+      result = nil
+      @locker.lock(@lock_name, @options) do
+        result = block.call
+      end
+      result
+    end
+  end
+end
