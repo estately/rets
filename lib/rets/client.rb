58edddbadcf5549087a9cd95ca16692c843106dd
Trung Duc Tran <trung@tdtran.org>
2014-09-23 19:33:05 +0200

    Fix test failure

--- lib/rets/client.rb
+++ lib/rets/client.rb
@@ -341,7 +341,8 @@ module Rets
       def self.check(response)
         # some RETS servers returns HTTP code 412 when session cookie expired, yet the response body
         # passes XML check. We need to special case for this situation.
-        if response.status_code == 412
+        # This method is also called from multipart.rb where there are headers and body but no status_code
+        if response.respond_to?(:status_code) && response.status_code == 412
           raise HttpError, "HTTP status: #{response.status_code}, body: #{response.body}"
         end
 
