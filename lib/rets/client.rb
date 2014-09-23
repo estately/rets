9094087d26c75517fc7bf1a62a3f22fb69aa41a2
Trung Duc Tran <trung@tdtran.org>
2014-09-23 18:31:52 +0200

    Need to special case for http code 412

--- lib/rets/client.rb
+++ lib/rets/client.rb
@@ -339,6 +339,12 @@ module Rets
 
     class ErrorChecker
       def self.check(response)
+        # some RETS servers returns HTTP code 412 when session cookie expired, yet the response body
+        # passes XML check. We need to special case for this situation.
+        if response.status_code == 412
+          raise HttpError, "HTTP status: #{response.status_code}, body: #{response.body}"
+        end
+
         # some RETS servers return success code in XML body but failure code 4xx in http status
         # If xml body is present we ignore http status
 
