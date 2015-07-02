e04e22da1ed92f7939975aee2deff40080a0656f
Hayden Faulds <fauldsh@gmail.com>
2015-07-02 12:03:13 +0100

    split out table classes

---
+++ lib/rets/metadata/table_factory.rb
@@ -0,0 +1,20 @@
+module Rets
+  module Metadata
+    class TableFactory
+      def self.build(table_fragment, resource)
+        if enum?(table_fragment)
+          LookupTable.new(table_fragment, resource)
+        else
+          Table.new(table_fragment, resource)
+        end
+      end
+
+      def self.enum?(table_fragment)
+        lookup_value   = table_fragment["LookupName"].strip
+        interpretation = table_fragment["Interpretation"].strip
+
+        interpretation =~ /Lookup/ && !lookup_value.empty?
+      end
+    end
+  end
+end
