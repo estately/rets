8ea3df30888581fc32a3ba0b2a3a6e5041de45a1
Hayden Faulds <fauldsh@gmail.com>
2015-07-07 20:04:10 +0100

    user attr_reader not attr_accessor

--- lib/rets/metadata/lookup_type.rb
+++ lib/rets/metadata/lookup_type.rb
@@ -1,12 +1,12 @@
 module Rets
   module Metadata
     class LookupType
-      attr_accessor :long_value, :short_value, :value
+      attr_reader :long_value, :short_value, :value
 
       def initialize(lookup_type_fragment)
-        self.value       = lookup_type_fragment["Value"]
-        self.short_value = lookup_type_fragment["ShortValue"]
-        self.long_value  = lookup_type_fragment["LongValue"]
+        @value       = lookup_type_fragment["Value"]
+        @short_value = lookup_type_fragment["ShortValue"]
+        @long_value  = lookup_type_fragment["LongValue"]
       end
 
       # Print the tree to a file
