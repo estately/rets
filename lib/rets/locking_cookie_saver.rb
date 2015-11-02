37f0ef2bdbab820afd361abf9b7a7439f935081c
Kyle Rippey <kylerippey@gmail.com>
2015-11-02 13:28:27 -0800

    Added timeouts to lock acquisition

--- lib/rets/locking_cookie_saver.rb
+++ lib/rets/locking_cookie_saver.rb
@@ -1,13 +1,19 @@
 module Rets
   class LockingCookieSaver < HTTPClient::WebAgentSaver
     def save(io, jar)
-      io.flock(File::LOCK_EX) if io.respond_to?(:flock)
+      Timeout::timeout(lock_timeout) { io.flock(File::LOCK_EX) } if io.respond_to?(:flock)
       super
     end
 
     def load(io, jar)
-      io.flock(File::LOCK_SH) if io.respond_to?(:flock)
+      Timeout::timeout(lock_timeout) { io.flock(File::LOCK_SH) } if io.respond_to?(:flock)
       super
+    end
+
+    private
+
+    def lock_timeout
+      1
     end
   end
 end
--- test/test_locking_cookie_saver.rb
+++ test/test_locking_cookie_saver.rb
@@ -25,4 +25,21 @@ class TestLockingCookieSaver < MiniTest::Test
       @cookie_saver.save(f, @cookie_jar)
     end
   end
+
+  def test_times_out_if_lock_cannot_be_acquired
+    Rets::LockingCookieSaver.any_instance.stubs(:lock_timeout).returns(0.01)
+
+    assert_raises Timeout::Error do
+      SlowFile.open('/dev/null', 'w') do |f|
+        @cookie_saver.save(f, @cookie_jar)
+      end
+    end
+  end
+end
+
+class SlowFile < File
+  def flock(lock_mode)
+    sleep 2
+    super
+  end
 end
