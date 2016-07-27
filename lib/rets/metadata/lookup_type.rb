e43ecb6bc7fe72e72e5416eb981a5253318d6b82
Hayden Faulds <fauldsh@gmail.com>
2016-07-27 15:20:04 +0100

    strip trailing whitespace from lookup types

--- lib/rets/metadata/lookup_type.rb
+++ lib/rets/metadata/lookup_type.rb
@@ -4,8 +4,8 @@ module Rets
       attr_reader :long_value, :value
 
       def initialize(lookup_type_fragment)
-        @value       = lookup_type_fragment["Value"]
-        @long_value  = lookup_type_fragment["LongValue"]
+        @value       = lookup_type_fragment["Value"].rstrip
+        @long_value  = lookup_type_fragment["LongValue"].rstrip
       end
 
       # Print the tree to a file
